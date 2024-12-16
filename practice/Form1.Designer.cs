ced31b66dded54e048a3da1113b2c49267a42559
kyeob1107 <koak1107@gmail.com>
2024-12-16 14:04:12 +0900

    Add project files.

---
+++ practice/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_print = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_print
+            // 
+            this.textBox_print.Location = new System.Drawing.Point(12, 35);
+            this.textBox_print.Multiline = true;
+            this.textBox_print.Name = "textBox_print";
+            this.textBox_print.Size = new System.Drawing.Size(726, 213);
+            this.textBox_print.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cascadia Mono", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Result:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 260);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_print);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_print;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ practice/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 이곳에 소스코드를 작성
+
+            //textBox_print.Text = "어떤 결과가 나올까?";
+
+            byte call_total_count = 30;
+            short a_point_height_meter = 2744;
+            int solar_diameter_kilometer = 1392684;
+            float exchange_rate_dollar_to_won = 1436.54f;
+            double pie_to_15 = 3.141592653589793;
+            decimal jupiter_mass_ton = 1.899m * 1000000000000000000000000m;
+
+            // byte
+            textBox_print.Text += call_total_count.GetType();
+            textBox_print.Text += " call_total_count: ";
+            textBox_print.Text += call_total_count.ToString();
+            // short
+            textBox_print.Text += "\r\n";
+            textBox_print.Text += a_point_height_meter.GetType();
+            textBox_print.Text += " a_point_height_meter: ";
+            textBox_print.Text += a_point_height_meter.ToString();
+            // int
+            textBox_print.Text += "\r\n";
+            textBox_print.Text += solar_diameter_kilometer.GetType();
+            textBox_print.Text += " solar_diameter_kilometer: ";
+            textBox_print.Text += solar_diameter_kilometer.ToString();
+            // float
+            textBox_print.Text += "\r\n";
+            textBox_print.Text += exchange_rate_dollar_to_won.GetType();
+            textBox_print.Text += " exchange_rate_dollar_to_won: ";
+            textBox_print.Text += exchange_rate_dollar_to_won.ToString();
+            // double
+            textBox_print.Text += "\r\n";
+            textBox_print.Text += pie_to_15.GetType();
+            textBox_print.Text += " pie_to_15: ";
+            textBox_print.Text += pie_to_15.ToString();
+            // decimal
+            textBox_print.Text += "\r\n";
+            textBox_print.Text += jupiter_mass_ton.GetType();
+            textBox_print.Text += " jupiter_mass_ton: ";
+            textBox_print.Text += jupiter_mass_ton.ToString();
+        }
+    }
+}
