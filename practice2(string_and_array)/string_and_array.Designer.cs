2d3b02ba7d590218b1bb61be7aca18dea088ef7b
kyeob1107 <koak1107@gmail.com>
2024-12-17 11:55:53 +0900

    두번째 실습 문자열과 배열로 배열에 문자열 메소드 결과물들을 넣어 출력하는 작업물 최초

---
+++ practice2(string_and_array)/string_and_array.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace practice2_string_and_array_
+{
+    partial class string_and_array
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_print = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_print
+            // 
+            this.textBox_print.Location = new System.Drawing.Point(12, 34);
+            this.textBox_print.Multiline = true;
+            this.textBox_print.Name = "textBox_print";
+            this.textBox_print.Size = new System.Drawing.Size(776, 404);
+            this.textBox_print.TabIndex = 0;
+            // 
+            // string_and_array
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_print);
+            this.Name = "string_and_array";
+            this.Text = "string and array";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_print;
+    }
+}
+
---
+++ practice2(string_and_array)/string_and_array.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice2_string_and_array_
+{
+    public partial class string_and_array : Form
+    {
+        string[] string_array = new string[10];
+        string s1 = "동해 물과 백두산이";
+        string s2 = "토요일에 먹는 토마토";
+        string s3 = "질서있는 퇴장";
+        string s4 = "그 사람의 그림자는 그랬다";
+        string s5 = "삼성 갤럭시";
+        string s6 = "오늘은 왠지 더 배고프다";
+        string s7 = "이름, 나이, 전화번호";
+        string s8 = "우리 나라 만세";
+
+        public string_and_array()
+        {
+            InitializeComponent();
+            string_array[0] = s1.IndexOf("백두산").ToString();
+            string_array[1] = s2.LastIndexOf("토").ToString();
+            string_array[2] = s3.Contains("퇴").ToString();
+            string_array[3] = s4.Replace("그", "이");
+            string_array[4] = s5.Insert(s5.IndexOf(" ") + 1, "애플 ");
+            string_array[5] = s6.Remove(s6.IndexOf("더"));
+            string[] split_temp = s7.Split(',');
+            string_array[6] = split_temp[0];
+            string_array[7] = split_temp[1].Replace(" ", "");
+            string_array[8] = split_temp[2].Replace(" ", "");
+            string_array[9] = s8.Substring(s8.IndexOf(" ") + 1, 2);
+
+            foreach (string sub in string_array)
+            {
+                textBox_print.Text += sub + "\r\n";
+            }
+        }
+    }
+}
