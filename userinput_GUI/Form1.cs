99b5ccb50000b209a03c906906bec1dea5fe8ca6
kyeob1107 <koak1107@gmail.com>
2024-12-19 08:41:49 +0900

    사용자가 입력하는 방식으로 GUI 수정한 버전(세세한 것은 안되어있고 그냥 단순하게 버튼 누르면 textBox_input 값이 textBox_result값으로 복사되게 해둔게 전부, 추후 필요에 따라 update 예정)

---
+++ userinput_GUI/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace userinput_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button_input_Click(object sender, EventArgs e)
+        {
+            textBox_result.Text = textBox_input.Text;
+        }
+    }
+}
