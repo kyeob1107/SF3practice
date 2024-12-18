d61907feefb948d6a85319654ea97fd0a79471ce
kyeob1107 <koak1107@gmail.com>
2024-12-18 10:42:03 +0900

    실습4 입력부분을 텍스트박스를 이용해서 추가로 입력받고 계산 - 계산 누를 시 좀 더 간단히 코드를 변환하고 싶은데 아직 불가해서 시간 애매할듯하여 일단 여기서 keep

--- practice4(function)/function.Designer.cs
+++ practice4(function)/function.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             this.textBox_print = new System.Windows.Forms.TextBox();
+            this.div_x = new System.Windows.Forms.TextBox();
+            this.div_y = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox_print
@@ -39,11 +44,60 @@
             this.textBox_print.Size = new System.Drawing.Size(732, 323);
             this.textBox_print.TabIndex = 0;
             // 
+            // div_x
+            // 
+            this.div_x.Location = new System.Drawing.Point(121, 29);
+            this.div_x.Name = "div_x";
+            this.div_x.Size = new System.Drawing.Size(100, 25);
+            this.div_x.TabIndex = 1;
+            this.div_x.Text = "7";
+            // 
+            // div_y
+            // 
+            this.div_y.Location = new System.Drawing.Point(326, 29);
+            this.div_y.Name = "div_y";
+            this.div_y.Size = new System.Drawing.Size(100, 25);
+            this.div_y.TabIndex = 2;
+            this.div_y.Text = "5";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "나누어질 수";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(248, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "나누는 수";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(455, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 43);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "계산";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // function
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.div_y);
+            this.Controls.Add(this.div_x);
             this.Controls.Add(this.textBox_print);
             this.Name = "function";
             this.Text = "div_fnct";
@@ -55,6 +109,11 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox_print;
+        private System.Windows.Forms.TextBox div_x;
+        private System.Windows.Forms.TextBox div_y;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- practice4(function)/function.cs
+++ practice4(function)/function.cs
@@ -14,8 +14,10 @@ namespace practice4_function_
     {
         public function()
         {
+            // 처음 실행시 form이 만들어지는 어떤 것이라는 좀 특수한 것이라고 함
             InitializeComponent();
-            int[] value = Div_mod(7, 5);
+            //int[] value = Div_mod(7, 5);
+            int[] value = Div_mod(int.Parse(div_x.Text), int.Parse(div_y.Text));
             textBox_print.Text = "몫은 " + value[0].ToString();
             textBox_print.Text += ", 나머지는 " + value[1].ToString();
         }
@@ -27,6 +29,16 @@ namespace practice4_function_
             result[1] = x % y;
 
             return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //function(); 
+            //form(function)그냥은 못쓰고 따로 객체로 만들어야 가능하다고 해서
+            //일단 그렇게 알고 패스
+            int[] value = Div_mod(int.Parse(div_x.Text), int.Parse(div_y.Text));
+            textBox_print.Text = "몫은 " + value[0].ToString();
+            textBox_print.Text += ", 나머지는 " + value[1].ToString();
         }
     }
 }
