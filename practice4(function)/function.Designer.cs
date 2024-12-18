be7085a14d0ce289d33c9fd5b972c3c60e3f090d
kyeob1107 <koak1107@gmail.com>
2024-12-18 10:22:19 +0900

    실습4 함수 실습문제 - 가장 기초적으로 정해둔 임의의 입력값으로 함수작동확인 후 하나로 출력하는 방식

---
+++ practice4(function)/function.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace practice4_function_
+{
+    partial class function
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_print = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox_print
+            // 
+            this.textBox_print.Location = new System.Drawing.Point(31, 79);
+            this.textBox_print.Multiline = true;
+            this.textBox_print.Name = "textBox_print";
+            this.textBox_print.Size = new System.Drawing.Size(732, 323);
+            this.textBox_print.TabIndex = 0;
+            // 
+            // function
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox_print);
+            this.Name = "function";
+            this.Text = "div_fnct";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_print;
+    }
+}
+
---
+++ practice4(function)/function.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice4_function_
+{
+    public partial class function : Form
+    {
+        public function()
+        {
+            InitializeComponent();
+            int[] value = Div_mod(7, 5);
+            textBox_print.Text = "몫은 " + value[0].ToString();
+            textBox_print.Text += ", 나머지는 " + value[1].ToString();
+        }
+
+        public int[] Div_mod(int x, int y)
+        {
+            int[] result = new int[2];
+            result[0] = x / y;
+            result[1] = x % y;
+
+            return result;
+        }
+    }
+}
