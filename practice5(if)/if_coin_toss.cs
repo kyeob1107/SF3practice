3523f4a031764ae18b48d0b7546deebe4370e4d5
kyeob1107 <koak1107@gmail.com>
2024-12-18 21:36:39 +0900

    실습5 주석추가

--- practice5(if)/if_coin_toss.cs
+++ practice5(if)/if_coin_toss.cs
@@ -16,7 +16,11 @@ namespace practice5_if_
         {
             InitializeComponent();
             string result_text;
+            
+            // 우선 동전을 True로 예측하는 것으로 설정
             bool prediction_1 = true;
+
+            // 결과 확인 및 출력
             if (Coin_toss_result(prediction_1))
             {
                 result_text = "승리";
@@ -30,11 +34,14 @@ namespace practice5_if_
         }
 
         public bool Coin_toss_result(bool prediction)
-        {
+        {   
+            // 난수 객체 생성 및 이용하여 정수 생성
             Random random = new Random();
             int coin_pre_process = random.Next();
             bool coin;
             bool result;
+
+            //랜덤 값을 동전 상태(bool)로 정의
             if (coin_pre_process % 2 == 0)
             {
                 coin = false;
@@ -44,6 +51,7 @@ namespace practice5_if_
                 coin = true;
             }
 
+            // 예측결과 bool로 저장
             result = (coin == prediction); // 괄호 안쳐도 작동은 하는듯함
 
             return result;
