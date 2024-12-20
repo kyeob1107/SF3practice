98f50a4cbe619a9752eab3e6582f11582ce5fba1
kyeob1107 <koak1107@gmail.com>
2024-12-20 14:14:43 +0900

    실습9 가위바위보 만들기(원래는 while실습하려했으나 적절한 문제를 만들지 못해서 그냥 마저하고 추가로 for실습을 while로 변경해서 추가로 할 예정)

---
+++ practice9(rsp)/rsp.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace practice9_while_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.button_input = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_user_wins = new System.Windows.Forms.Label();
+            this.label_pc_wins = new System.Windows.Forms.Label();
+            this.button_scissors = new System.Windows.Forms.Button();
+            this.button_rock = new System.Windows.Forms.Button();
+            this.button_paper = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox_result.Location = new System.Drawing.Point(12, 90);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.ReadOnly = true;
+            this.textBox_result.Size = new System.Drawing.Size(343, 224);
+            this.textBox_result.TabIndex = 2;
+            // 
+            // button_input
+            // 
+            this.button_input.Location = new System.Drawing.Point(285, 9);
+            this.button_input.Name = "button_input";
+            this.button_input.Size = new System.Drawing.Size(75, 42);
+            this.button_input.TabIndex = 1;
+            this.button_input.Text = "restart";
+            this.button_input.UseVisualStyleBackColor = true;
+            this.button_input.Click += new System.EventHandler(this.button_input_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gulim", 11F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "가위, 바위, 보를 선택해주세요";
+            // 
+            // label_user_wins
+            // 
+            this.label_user_wins.AutoSize = true;
+            this.label_user_wins.Location = new System.Drawing.Point(13, 57);
+            this.label_user_wins.Name = "label_user_wins";
+            this.label_user_wins.Size = new System.Drawing.Size(125, 15);
+            this.label_user_wins.TabIndex = 5;
+            this.label_user_wins.Text = "사용자 승리 수: 0";
+            // 
+            // label_pc_wins
+            // 
+            this.label_pc_wins.AutoSize = true;
+            this.label_pc_wins.Location = new System.Drawing.Point(183, 57);
+            this.label_pc_wins.Name = "label_pc_wins";
+            this.label_pc_wins.Size = new System.Drawing.Size(125, 15);
+            this.label_pc_wins.TabIndex = 5;
+            this.label_pc_wins.Text = "컴퓨터 승리 수: 0";
+            // 
+            // button_scissors
+            // 
+            this.button_scissors.Location = new System.Drawing.Point(16, 31);
+            this.button_scissors.Name = "button_scissors";
+            this.button_scissors.Size = new System.Drawing.Size(75, 23);
+            this.button_scissors.TabIndex = 6;
+            this.button_scissors.Text = "가위";
+            this.button_scissors.UseVisualStyleBackColor = true;
+            this.button_scissors.Click += new System.EventHandler(this.button_scissors_Click);
+            // 
+            // button_rock
+            // 
+            this.button_rock.Location = new System.Drawing.Point(102, 31);
+            this.button_rock.Name = "button_rock";
+            this.button_rock.Size = new System.Drawing.Size(75, 23);
+            this.button_rock.TabIndex = 6;
+            this.button_rock.Text = "바위";
+            this.button_rock.UseVisualStyleBackColor = true;
+            this.button_rock.Click += new System.EventHandler(this.button_rock_Click);
+            // 
+            // button_paper
+            // 
+            this.button_paper.Location = new System.Drawing.Point(183, 31);
+            this.button_paper.Name = "button_paper";
+            this.button_paper.Size = new System.Drawing.Size(75, 23);
+            this.button_paper.TabIndex = 6;
+            this.button_paper.Text = "보";
+            this.button_paper.UseVisualStyleBackColor = true;
+            this.button_paper.Click += new System.EventHandler(this.button_paper_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 326);
+            this.Controls.Add(this.button_paper);
+            this.Controls.Add(this.button_rock);
+            this.Controls.Add(this.button_scissors);
+            this.Controls.Add(this.label_pc_wins);
+            this.Controls.Add(this.label_user_wins);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_input);
+            this.Controls.Add(this.textBox_result);
+            this.Name = "Form1";
+            this.Text = "가위_바위_보";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.Button button_input;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_user_wins;
+        private System.Windows.Forms.Label label_pc_wins;
+        private System.Windows.Forms.Button button_scissors;
+        private System.Windows.Forms.Button button_rock;
+        private System.Windows.Forms.Button button_paper;
+    }
+}
+
---
+++ practice9(rsp)/rsp.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice9_while_
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        //enum rock_scissors_parper
+        //{
+        //    Rock = 0,
+        //    Scissors = 2,
+        //    Paper = 5
+
+        //}
+        // 바위 = 0
+        // 가위 = 2
+        // 보 = 5
+        int user_state;
+        int pc;
+        int win_count_user = 0;
+        int win_count_pc = 0;
+
+        public Form1()
+
+        {
+            InitializeComponent();
+            textBox_result.Text = "";
+
+        }
+        
+        //다시시작
+        private void button_input_Click(object sender, EventArgs e)
+        {
+            win_count_user = 0;
+            win_count_pc = 0;
+            label_user_wins.Text = $"사용자 승리 수: {win_count_user}";
+            label_pc_wins.Text = $"컴퓨터 승리 수: {win_count_pc}";
+            textBox_result.Text = "";
+        }
+
+        private void button_scissors_Click(object sender, EventArgs e)
+        {
+            user_state = 2;
+            textBox_result.Text ="사용자가 가위를 냈습니다\r\n";
+            Final_process();
+
+
+        }
+
+        private void button_rock_Click(object sender, EventArgs e)
+        {
+            user_state = 0;
+            textBox_result.Text = "사용자가 바위를 냈습니다\r\n";
+            Final_process();
+
+        }
+
+        private void button_paper_Click(object sender, EventArgs e)
+        {
+            user_state = 5;
+            textBox_result.Text = "사용자가 보를 냈습니다\r\n";
+            Final_process();
+        }
+
+        void Final_process()
+        {
+            pc = Computer_random_rsp();
+            Check_result(user_state, pc);
+            label_user_wins.Text = $"사용자 승리 수: {win_count_user}";
+            label_pc_wins.Text = $"컴퓨터 승리 수: {win_count_pc}";
+            if (win_count_user > 2 | win_count_pc > 2)
+            {
+                textBox_result.Text += "게임이 끝났습니다\r\n";
+                if (win_count_pc > 2)
+                {
+                    textBox_result.Text += "컴퓨터가 최종승리하였습니다";
+                    MessageBox.Show("컴퓨터가 최종승리하였습니다");
+                }
+                else
+                {
+                    textBox_result.Text += "사용자가 최종승리하였습니다";
+                    MessageBox.Show("사용자가 최종승리하였습니다");
+                }
+            }
+        }
+
+        int Computer_random_rsp()
+        {
+            //컴퓨터가 랜덤하게 내기
+            int pre_pc_state = rand.Next(3); // 랜덤하게 컴퓨터 낼 것 정하는 값
+            switch (pre_pc_state)
+            {
+                case 0:
+                    textBox_result.Text += "컴퓨터가 바위를 냈습니다\r\n";
+                    return 0;
+
+                case 1:
+                    textBox_result.Text += "컴퓨터가 가위를 냈습니다\r\n";
+                    return 2;
+
+                default:
+                    textBox_result.Text += "컴퓨터가 보를 냈습니다\r\n";
+                    return 5;
+
+            }
+        }
+
+        void Check_result(int user, int computer)
+        {
+            //비겼을때
+            if (user == computer)
+            {
+                textBox_result.Text += "비겼습니다\r\n";
+            }
+            // 바위와 보가 나왔을 때
+            else if (user + computer == 5)
+            {
+                if (user == 0)
+                {
+                    win_pc();
+                }
+                else
+                {
+                    win_user();
+                }
+            }
+            //그외 일반적인 상황
+            else
+            {
+                if (user > computer)
+                {
+                    win_pc();
+                }
+                else
+                {
+                    win_user();
+                }
+            }
+        }
+        void win_pc()
+        {
+            win_count_pc += 1;
+            textBox_result.Text += "컴퓨터가 이겼습니다\r\n";
+        }
+
+        void win_user()
+        {
+            win_count_user += 1;
+            textBox_result.Text += "사용자가 이겼습니다\r\n";
+        }
+
+    }
+}
