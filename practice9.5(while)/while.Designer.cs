948c767cd2dcc58d46cf6f1802cbe3fbbb210b25
kyeob1107 <koak1107@gmail.com>
2024-12-23 08:59:16 +0900

    실습9.5 실습8의 for문으로 구현한 것을 while문으로 변경한 버전

---
+++ practice9.5(while)/while.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace practice9._5_while_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.textBox_result = new System.Windows.Forms.TextBox();
+            this.button_input = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Location = new System.Drawing.Point(12, 59);
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(259, 25);
+            this.textBox_input.TabIndex = 0;
+            // 
+            // textBox_result
+            // 
+            this.textBox_result.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox_result.Location = new System.Drawing.Point(13, 91);
+            this.textBox_result.Multiline = true;
+            this.textBox_result.Name = "textBox_result";
+            this.textBox_result.ReadOnly = true;
+            this.textBox_result.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_result.Size = new System.Drawing.Size(343, 224);
+            this.textBox_result.TabIndex = 2;
+            // 
+            // button_input
+            // 
+            this.button_input.Location = new System.Drawing.Point(281, 59);
+            this.button_input.Name = "button_input";
+            this.button_input.Size = new System.Drawing.Size(75, 23);
+            this.button_input.TabIndex = 1;
+            this.button_input.Text = "Input";
+            this.button_input.UseVisualStyleBackColor = true;
+            this.button_input.Click += new System.EventHandler(this.button_input_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gulim", 11F);
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "학생수를 입력해주세요(숫자만)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 326);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_input);
+            this.Controls.Add(this.textBox_result);
+            this.Controls.Add(this.textBox_input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.TextBox textBox_result;
+        private System.Windows.Forms.Button button_input;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ practice9.5(while)/while.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// 실습8의 for로 구현한 것 while로 수정
+
+namespace practice9._5_while_
+{
+    public partial class Form1 : Form
+    {
+        int students_number;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button_input_Click(object sender, EventArgs e)
+        {
+            //텍스트박스 입력으로 숫자받음
+            if (int.TryParse(textBox_input.Text, out students_number))
+            {
+                int[] scores = Give_student_score(students_number);
+                student_name_score_print(scores);
+            }
+            //입력값이 숫자가 아닐경우
+            else
+            {
+                textBox_result.Text = $"숫자가 아닌 값을 입력하였습니다: {textBox_input.Text}";
+                return;
+            }
+        }
+
+        //랜덤하게 점수 부여함수
+        int[] Give_student_score(int number)
+        {
+            int[] student_scores = new int[students_number + 1];
+            //랜덤 객체 생성
+            Random random = new Random();
+            //반복문으로 학생수만큼 점수부여
+            int i = 1; // while을 위한 정수
+            //for (int i = 1; i < students_number + 1; i++)
+            //{
+            //    //Random random = new Random();
+            //    student_scores[i] = random.Next(101); //101미만의 정수
+            //}
+            while(i < students_number + 1)
+            {
+                student_scores[i] = random.Next(101); //101미만의 정수
+                i++;
+            }
+
+            return student_scores;
+        }
+
+        //출력함수
+        void student_name_score_print(int[] scores)
+        {
+            //textBox_result 초기화
+            textBox_result.Text = "";
+
+            //반복문을 통해 배열의 데이터 정해진 틀로 출력
+            for (int i = 1; i < students_number + 1; i++)
+            {
+                textBox_result.Text += $"학생{i}의 점수: {scores[i]}점\r\n";
+            }
+        }
+    }
+}
