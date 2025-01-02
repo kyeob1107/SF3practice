bb98b915fa378588c27c69181c63c9f587d99ecd
kyeob1107 <koak1107@gmail.com>
2025-01-02 11:57:38 +0900

    실습14 ref와 out을 이용함 참조 연습 및 ref와 out 사용 차이점 확인하기

---
+++ practice14(reference)/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("practice14_reference_")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("practice14_reference_")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2cb2996c-16a0-4356-80bb-c50f201989c3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ practice14(reference)/RefOut.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice14_reference_
+{
+    public partial class RefOut : Form
+    {
+        //int[] numbers;
+        int[] numbers;
+        int[] numbers2;
+        
+
+
+        public RefOut()
+        {
+            InitializeComponent();
+            numbers = new int[10];
+        }
+
+        void Practice_ref(ref int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i + 1;
+            }
+        }
+
+        void Practice_out(out int[] numbers2, int size)
+        {
+            numbers2 = new int[size];
+            for (int i = 0; i < numbers2.Length; i++)
+            {
+                numbers2[i] = i + 1;
+            } 
+        }
+
+        private void button_ref_Click(object sender, EventArgs e)
+        {
+            Practice_ref(ref numbers);
+            textBox_result.Text += "==========ref 실행한 결과입니다============\r\n";
+            foreach (int number in numbers)
+            {
+                textBox_result.Text += $"{number} \r\n";
+            }
+        }
+
+        private void button_out_Click(object sender, EventArgs e)
+        {
+            Practice_out(out numbers2, 10);
+            textBox_result.Text += "==========out 실행한 결과입니다============\r\n";
+            foreach (int number in numbers2)
+            {
+                textBox_result.Text += $"{number} \r\n";
+            }
+        }
+    }
+}
