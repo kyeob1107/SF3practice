4c6ab68693007983da45dc56d99a4cfefd5d1db4
kyeob1107 <koak1107@gmail.com>
2025-01-03 13:26:11 +0900

    실습16 제네릭 딕셔너리 연습 로그인정보 담긴 파일 열어서 읽고 그것으로 로그인과 비교해서 로그인 성공 여부 판단 및 정보 출력

---
+++ practice16(generic_dictionary)/generic_dictionary.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace practice16_generic_dictionary_
+{
+    partial class generic_dictionary
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_input_id = new System.Windows.Forms.TextBox();
+            this.button_open = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_login = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_input_pw = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox_default_path = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox_input_id
+            // 
+            this.textBox_input_id.Location = new System.Drawing.Point(54, 60);
+            this.textBox_input_id.Name = "textBox_input_id";
+            this.textBox_input_id.Size = new System.Drawing.Size(222, 25);
+            this.textBox_input_id.TabIndex = 0;
+            // 
+            // button_open
+            // 
+            this.button_open.Location = new System.Drawing.Point(282, 12);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(75, 34);
+            this.button_open.TabIndex = 1;
+            this.button_open.Text = "Open";
+            this.button_open.UseVisualStyleBackColor = true;
+            this.button_open.Click += new System.EventHandler(this.button_open_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gulim", 11F);
+            this.label1.Location = new System.Drawing.Point(12, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID";
+            // 
+            // button_login
+            // 
+            this.button_login.Location = new System.Drawing.Point(282, 61);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(75, 60);
+            this.button_login.TabIndex = 4;
+            this.button_login.Text = "Login";
+            this.button_login.UseVisualStyleBackColor = true;
+            this.button_login.Click += new System.EventHandler(this.button_login_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gulim", 11F);
+            this.label2.Location = new System.Drawing.Point(12, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PW";
+            // 
+            // textBox_input_pw
+            // 
+            this.textBox_input_pw.Location = new System.Drawing.Point(54, 95);
+            this.textBox_input_pw.Name = "textBox_input_pw";
+            this.textBox_input_pw.PasswordChar = '●';
+            this.textBox_input_pw.Size = new System.Drawing.Size(222, 25);
+            this.textBox_input_pw.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gulim", 11F);
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "default_path";
+            // 
+            // comboBox_default_path
+            // 
+            this.comboBox_default_path.FormattingEnabled = true;
+            this.comboBox_default_path.Items.AddRange(new object[] {
+            "바탕화면",
+            "문서",
+            "프로젝트실행경로"});
+            this.comboBox_default_path.Location = new System.Drawing.Point(124, 9);
+            this.comboBox_default_path.Name = "comboBox_default_path";
+            this.comboBox_default_path.Size = new System.Drawing.Size(121, 23);
+            this.comboBox_default_path.TabIndex = 7;
+            // 
+            // generic_dictionary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 133);
+            this.Controls.Add(this.comboBox_default_path);
+            this.Controls.Add(this.textBox_input_pw);
+            this.Controls.Add(this.button_login);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_open);
+            this.Controls.Add(this.textBox_input_id);
+            this.Name = "generic_dictionary";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_input_id;
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_login;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_input_pw;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox_default_path;
+    }
+}
+
---
+++ practice16(generic_dictionary)/generic_dictionary.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace practice16_generic_dictionary_
+{
+    public partial class generic_dictionary : Form
+    {
+        string filePath;
+        IDictionary<string, string> loginInfo;
+        IDictionary<string, string> userInfo;
+        public generic_dictionary()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button_open_Click(object sender, EventArgs e)
+        {
+            filePath = OpenFileDialog_action();
+            ReadFile(filePath, out loginInfo, out userInfo);
+
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            // 데이터 똑바로 들어갔나 확인용
+            //foreach(KeyValuePair<string, string> user in userInfo) //var로 해도 되는듯
+            //{
+            //    MessageBox.Show($"{user.Key}: {user.Value}");
+            //}
+            string input_id;
+            string input_pw;
+            input_id = textBox_input_id.Text;
+            input_pw = textBox_input_pw.Text;
+
+            if (loginInfo.TryGetValue(input_id, out string value_pw))
+            {
+                if (input_pw == value_pw)
+                {
+                    string messageText = $"{input_id}님 환영합니다\r\n";
+                    //MessageBox.Show(userInfo[input_id].Length.ToString());
+                    messageText += $"({TransPhoneNumber(userInfo[input_id])})";
+                    MessageBox.Show(messageText);
+                }
+                else MessageBox.Show("비밀번호가 틀렸습니다");
+            }
+            else MessageBox.Show("아이디가 존재하지 않습니다");
+
+        }
+
+        string OpenFileDialog_action()
+        {
+            string selectedFilePath = "";
+            try
+            {
+                //인스턴스 생성
+                OpenFileDialog ofd = new OpenFileDialog();
+
+                // 사용할 필터(확장자) 설정
+                List<string> filterList = new List<string>
+                {
+                    "Text Files (*.txt)|*.txt;",
+                    "|CSV Files (*.csv)|*.csv;",
+                    "|All Files (*.*)|*.*;"
+                };
+
+                // 필터(확장자 적용)
+                foreach (string filter in filterList)
+                { ofd.Filter += filter; }
+
+                // 또는 아래와 같이 한꺼번에 Supported Image File로 표시할 수 있다.
+                //ofd.Filter = "Supported Image File|*jpg;*.jpeg;*.bmp;*.png;*.dib;*.gif;*.tif;";
+
+                ofd.AddExtension = false;                    // 확장자 자동 추가
+                ofd.CheckFileExists = true;                  // 파일 존재하지 않을 경우 메시지 표시 여부
+                ofd.CheckPathExists = true;                  // 경로 존재하지 않을 경우 메시지 표시 여부    
+                ofd.Multiselect = false;                     // 복수 선택
+
+                ofd.ShowHelp = true;                         // Help 버튼 표시 여부
+                ofd.ShowReadOnly = false;                    // 읽기 전용 체크박스 표시 여부
+                ofd.ReadOnlyChecked = false;                 // 읽기 전용항목 체크 여부
+                ofd.SupportMultiDottedExtensions = true;     // 여러개의 확장자를 표시하는지 여부(저장시 또한)
+                ofd.Title = "Select an file...";            // 타이틀 표시
+                ofd.ValidateNames = true;                    // 파일명의 유효성 검증
+
+                //초기 디렉터리 -> 기본 바탕화면으로 설정, 선택가능하도록해줬음
+                //함수화시켜주고 싶음
+                string selectDefaultPath = comboBox_default_path.Text;
+                switch (selectDefaultPath)
+                {
+                    case "바탕화면":
+                        ofd.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                        break;
+                    case "문서":
+                        ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                        break;
+                    case "프로젝트실행경로":
+                        ofd.InitialDirectory = @"";
+                        break;
+                    //case "마지막 사용한 경로":
+                    //    break;
+                    default:
+                        ofd.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                        break;
+                }
+                
+
+
+                if (ofd.ShowDialog() == DialogResult.OK)    // 다이얼로그 표시
+                {
+                    //Do Something
+                    selectedFilePath = ofd.FileName; // 선택된 파일 경로 사용
+                    //string selectedFileName = ofd.SafeFileName; // 선택된 파일 이름 사용
+                }
+
+                ofd.Dispose(); // OpenFileDialog 리소스 해제
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString(), "Sample");
+            }
+
+            return selectedFilePath;
+        }
+
+        void ReadFile(string filePath, 
+            out IDictionary<string, string> loginInfo, out IDictionary<string, string> userInfo)
+        {
+            loginInfo = new Dictionary<string, string>();
+            userInfo = new Dictionary<string, string>();
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] data = line.Trim(' ', '\r', '\n').Split(',');
+                        loginInfo.Add(data[0], data[1]);
+                        if (data.Length > 2) userInfo.Add(data[0], data[2]);
+                        else userInfo.Add(data[0], null);
+                        //userInfo.Add(data[0], string.IsNullOrEmpty(data[2]) ? null: data[2]);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+        
+        // 전화번호 길이별 출력
+        string TransPhoneNumber(string phoneNumber)
+        {
+            if (string.IsNullOrEmpty(phoneNumber))
+            {
+                return "전화번호 정보 없음";
+            }
+            else
+            {
+                switch (phoneNumber.Length)
+                {
+                    case 11:
+                        //return $"{phoneNumber:###-####-####}"; //이건 왜 똑바로 안되지..?
+                        return $"{phoneNumber.Substring(0, 3)}" +
+                            $"-{phoneNumber.Substring(3, 4)}" +
+                            $"-{phoneNumber.Substring(7)}";
+                    case 10:
+                        return $"{phoneNumber.Substring(0, 3)}" +
+                            $"-{phoneNumber.Substring(3, 3)}" +
+                            $"-{phoneNumber.Substring(6)}";
+                    case 8:
+                        return $"{phoneNumber.Substring(0, 4)}-{phoneNumber.Substring(4)}";
+                    case 7:
+                        return $"{phoneNumber.Substring(0, 3)}-{phoneNumber.Substring(3)}";
+                    default:
+                        return phoneNumber; // 기타 길이는 그대로 반환
+                }
+            }
+
+        }
+    }
+}
